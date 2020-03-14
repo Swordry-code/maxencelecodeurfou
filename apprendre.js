ab29467af71d96c5069da1d777e9663bf27f9c15
Swordry-code <57687324+Swordry-code@users.noreply.github.com>
2020-03-14 23:38:45 +0100

    V1.0

--- apprendre.js
+++ apprendre.js
@@ -1,6 +1,9 @@
 var bouttons = document.querySelectorAll("div.conteneur > div.bouttons > button");
 var resultat = document.getElementById('resultat')
-var ptsJoueur
+var ptsJoueur = 0;
+var ptsIA = 0;
+var historique = document.querySelectorAll("div.conteneur > div.historique > div.affichageHistorique > p")
+console.log(historique);
 
 for (let i = 0; i < bouttons.length; i++) {
   bouttons[i].addEventListener('click', function() {
@@ -17,14 +20,26 @@ for (let i = 0; i < bouttons.length; i++) {
 
     else if ((joueur === "pierre" && IA === "ciseaux") || (joueur === "ciseaux" && IA === "feuille") || (joueur === "feuille") && (IA === "pierre")) {
         resultat.innerHTML = "Gagné";
+        ptsJoueur += 1;
     }
 
     else {
         resultat.innerHTML = "Perdu";
+        ptsIA += 1;
     }
 
-    vs = document.getElementById('vs')
+    var vs = document.getElementById('vs')
     vs.style.display = "block";
+
+    var score = document.getElementById('score')
+    score.innerHTML = ptsJoueur+" - "+ptsIA;
+
+    historique[3].innerHTML = historique[2].innerHTML
+    historique[2].innerHTML = historique[1].innerHTML
+    historique[1].innerHTML = historique[0].innerHTML
+    historique[0].innerHTML = "Joueur : "+joueur+"<br>Robot : "+IA+"<br>"+resultat.innerHTML+" : "+ptsJoueur+" - "+ptsIA;
+
+
 
   })
 
